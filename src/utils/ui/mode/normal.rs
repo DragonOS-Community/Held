edd69e50653a20d1473760b8016691158fff8380
trv3wood <1841369469@qq.com>
2024-08-06 22:59:12 +0800

    add comment

--- src/utils/ui/mode/normal.rs
+++ src/utils/ui/mode/normal.rs
@@ -1,8 +1,31 @@
+// 为了不影响 Command 模式的功能，参考Vim源码单独实现临时的 Normal 模式用于演示
+
+// Normal模式下的状态机
+
+// 在 input_data() 中处理输入的数据，根据输入的数据进行状态转移，
+// 具体而言，根据输入数据及当前状态来更新状态机的参数，如命令字符，重复次数等
+
+// 在 handle() 中处理状态的转移，根据状态的变化执行相应的操作
+// handle() 是真正作用于 ui 和 buffer 的地方，可以在这里调用 buffer 的方法，更新 ui 的显示
+// 因此，NormalState 提供给 handle() 的参数应该具有足够的一般性，以适应不同的需求
+
+// 在 exit() 中处理状态的退出，清空状态
+
+// 由此为 ndw,ndd 等命令的实现提供了多重字符匹配之外的方案：
+// 状态机的下一个状态仅由输入 + 当前状态决定，避免了对输入的全局匹配
+
+// 另：静态 HashMap 比起 match 性能更好是真的吗？后续是否考虑更换为 HashMap？
+// 另：在现有框架下，增加一个新功能，需要在 input_data() 中增加一个分支，handle() 中增加一个分支
+// 是否有更简便的代码结构？
+
+// 参考：https://github.com/neovim/neovim/blob/master/src/nvim/normal.c#L89
+
 use lazy_static::lazy_static;
 
 use crate::utils::ui::event::KeyEventCallback;
 use crate::utils::ui::event::WarpUiCallBackType;
 use crate::utils::ui::uicore::UiCore;
+use crate::utils::ui::uicore::CONTENT_WINSIZE;
 use std::io;
 use std::sync::{Mutex, MutexGuard};
 
@@ -104,16 +127,16 @@ impl NormalState {
         self.cmdchar = Some('j');
     }
     pub fn exec_j_cmd(&mut self, ui: &mut MutexGuard<UiCore>) -> io::Result<WarpUiCallBackType> {
-        let exec_count = match self.count {
-            Some(count) => {
-                count.min(ui.buffer.line_count() - ui.cursor.y() as usize - 1 - ui.buffer.offset())
-            }
-            None => 1,
-        };
         let old_y = ui.cursor.y();
-        let new_y = ui.buffer.goto_line(old_y as usize + exec_count);
+        let old_abs_y = old_y + ui.buffer.offset() as u16;
+        let exec_count = match self.count {
+            Some(count) => count.min(ui.buffer.line_count() - old_abs_y as usize - 1),
+            None => 1, // goto_line 会自动处理最大值
+        };
+        let new_y = ui.buffer.goto_line(old_abs_y as usize + exec_count);
         ui.cursor.move_to_row(new_y)?;
         ui.cursor.highlight(Some(old_y))?;
+        ui.render_content(0, CONTENT_WINSIZE.read().unwrap().rows as usize)?;
         self.reset();
         return Ok(WarpUiCallBackType::None);
     }
@@ -122,14 +145,23 @@ impl NormalState {
     }
 
     pub fn exec_k_cmd(&mut self, ui: &mut MutexGuard<UiCore>) -> io::Result<WarpUiCallBackType> {
-        let exec_count = match self.count {
-            Some(count) => count.min(ui.cursor.y() as usize + ui.buffer.offset()),
-            None => 1,
-        };
         let old_y = ui.cursor.y();
-        let new_y = ui.buffer.goto_line(old_y as usize - exec_count);
+        let old_abs_y = old_y + ui.buffer.offset() as u16;
+        let exec_count = match self.count {
+            Some(count) => count.min(old_y as usize + ui.buffer.offset()),
+            None => {
+                if old_abs_y == 0 {
+                    0
+                } else {
+                    1
+                }
+            } // 如果在第一行，不再向上移动，防止溢出
+        };
+        let to_line = old_abs_y as usize - exec_count;
+        let new_y = ui.buffer.goto_line(to_line);
         ui.cursor.move_to_row(new_y)?;
         ui.cursor.highlight(Some(old_y))?;
+        ui.render_content(0, CONTENT_WINSIZE.read().unwrap().rows as usize)?;
         self.reset();
         return Ok(WarpUiCallBackType::None);
     }
@@ -152,7 +184,7 @@ impl NormalState {
             count = count * 10 + (data[0] - b'0') as usize;
             self.count = Some(count);
         }
-        self.count0 = true;
+        self.count0 = true; // 将后续输入的0作为执行次数的一部分
     }
 
     pub fn on_zero_clicked(&mut self) {

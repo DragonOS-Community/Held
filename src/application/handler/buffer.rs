117176ad18d44655b2db23ca293b76a35fd2dd59
GnoCiYeH <heyicong@dragonos.org>
2024-10-16 22:57:14 +0800

    insertMode添加tab处理

--- src/application/handler/buffer.rs
+++ src/application/handler/buffer.rs
@@ -1,12 +1,15 @@
-use crossterm::event::{KeyCode, KeyEvent};
+use crossterm::event::KeyCode;
 
-use crate::application::Application;
 use crate::errors::*;
+use crate::{application::Application, util::position::Position};
+
+use super::cursor;
 
 pub fn insert_char(app: &mut Application) -> Result<()> {
     if let Some(key) = app.monitor.last_key {
         if let KeyCode::Char(c) = key.code {
             app.workspace.current_buffer.as_mut().unwrap().insert(c);
+            cursor::move_right(app)?;
         }
     }
     Ok(())
@@ -15,6 +18,28 @@ pub fn insert_char(app: &mut Application) -> Result<()> {
 pub fn new_line(app: &mut Application) -> Result<()> {
     if let Some(ref mut buffer) = app.workspace.current_buffer {
         buffer.insert('\n');
+    }
+    Ok(())
+}
+
+pub fn insert_tab(app: &mut Application) -> Result<()> {
+    if let Some(buffer) = app.workspace.current_buffer.as_mut() {
+        let tab_len = app.perferences.borrow().tab_width();
+        let width = tab_len - (buffer.cursor.offset) % tab_len;
+        if app.perferences.borrow().soft_tab() {
+            let tab_str = " ".repeat(width);
+            buffer.insert(tab_str);
+            buffer.cursor.move_to(Position {
+                line: buffer.cursor.line,
+                offset: buffer.cursor.offset + width,
+            });
+        } else {
+            buffer.insert("\t");
+            buffer.cursor.move_to(Position {
+                line: buffer.cursor.line,
+                offset: buffer.cursor.offset + width,
+            });
+        }
     }
     Ok(())
 }
